d7d204f02864d09dcda46ef589cca069bb8f5093
QuangDao52 <quangdao5320@gmail.com>
2023-02-22 22:31:01 +0700

    update change password

--- app/src/main/java/com/example/shop_app/fragment/OnboardingFragment3.java
+++ app/src/main/java/com/example/shop_app/fragment/OnboardingFragment3.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.shop_app.activity.LoginActivity;
 import com.example.shop_app.activity.MainActivity;
 import com.example.shop_app.R;
 import com.example.shop_app.utils.DatalocalManager;
@@ -16,7 +17,7 @@ import com.example.shop_app.utils.DatalocalManager;
 
 public class OnboardingFragment3 extends Fragment {
 
-private Button btnstart;
+private Button btnstart,btn_login;
 private View mView;
 
     public OnboardingFragment3() {
@@ -29,7 +30,7 @@ private View mView;
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.onboading1, container, false);
         btnstart = mView.findViewById(R.id.started);
-
+        btn_login = mView.findViewById(R.id.btn_login);
         btnstart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,6 +40,16 @@ private View mView;
                 getActivity().startActivity(intent);
             }
         });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                DatalocalManager.setFirstInstalled(true);
+
+                getActivity().startActivity(intent);
+            }
+        });
+
         return mView;
     }
 }
